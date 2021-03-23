b714a8af81d787f3bdae1a81a6101cca0dbd584c
gupta_keshav_001378461 <gupta.k@husky.neu.edu>
2021-03-23 12:23:15 -0400

    Weather App completed deployment is left

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,25 @@
-const request = require('request')
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
 
-const url = 'http://api.weatherstack.com/current?access_key=3903ef8b71dde02aa708ae5cd1c7ba28&query=37.8267,-122.4233&units=f'
+const address = process.argv[2]
 
-request({url: url, json: true}, (error, response) =>{
-    if(error){
-        console.log('Unable to connect to weather service!')
-    } else {
-        console.log(response.body.current.weather_descriptions[0] +'. It is currently ' + response.body.current.temperature + ' degrees out. It feels like  ' 
-    + response.body.current.feelslike + ' degree temperature')
-    }
+if(!address){
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) =>{
+        if(error){
+            return console.log(error)
+        }
+        forecast(data.latitude, data.longitude,  (error, forecastdata) =>{
+    
+            if(error){
+                return console.log(error)
+            }
+        console.log(data.location)
+        console.log(forecastdata)
     })
+    })   
+}
 
-//Geocoding
-// Address -> Lat/Long -> Weather
 
-// const mapBoxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia2VzaGF2OTUiLCJhIjoiY2ttMTZvdDdkMHN0bjJ2cXN3anBmNGJscSJ9.KMLEb1Vh9wy8wK4iCNIs4g'
-
-// request ({url: mapBoxUrl, json: true}, (error, response) =>{
-   
-//     console.log(response.body.features[0].center[1], response.body.features[0].center[0])
-// })
+    
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+const forecast = (longitude, latitude, callback) =>{
+    const url ='http://api.weatherstack.com/current?access_key=3903ef8b71dde02aa708ae5cd1c7ba28&query='+ longitude + ','+ latitude + '&unit=f'
+    request({url: url, json: true}, (error, response) => {
+        if(error){
+           callback('Unable to connect to weather services!', undefined) 
+        }else if(response.body.error){
+            callback('Unable to find location, Try another search', undefined)
+        } else {
+            callback(undefined, {
+                Description: response.body.current.weather_descriptions[0],
+                temperature: response.body.current.temperature,
+                feelsLike: response.body.current.feelslike
+
+            })
+        }
+    })
+}
+
+module.exports = forecast
