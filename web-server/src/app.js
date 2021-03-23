890c06242a67f1d374ee75dfde81722af7019845
gupta_keshav_001378461 <gupta.k@husky.neu.edu>
2021-03-23 13:02:16 -0400

    Setup app for heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast.js')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for Express config
 const publicDirectory = path.join(__dirname,'../public')
@@ -96,6 +97,6 @@ res.render('404',{
 } )
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is listening on' + port)
 })
